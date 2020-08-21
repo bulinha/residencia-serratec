04713837e71105d2eb8a0fba3fc9ddfc176455c1
Luis Henrique M Carvalho <bulinha@gmail.com>
2020-08-21 11:38:03 -0300

    inclusao de categoria

---
+++ Livro/src/main/java/org/serratec/java2backend/livro/entidade/Categoria.java
@@ -0,0 +1,54 @@
+package org.serratec.java2backend.livro.entidade;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;	
+	
+	@Column(name="nome", nullable = false, length=20)
+	private String nome;
+	
+	
+	
+
+	public Categoria() {
+		super();
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	public Categoria(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
---
+++ Livro/src/main/java/org/serratec/java2backend/livro/repositorio/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package org.serratec.java2backend.livro.repositorio;
+
+import org.serratec.java2backend.livro.entidade.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
